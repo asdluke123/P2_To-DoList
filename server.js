27a1ba0423eb2ae0882990f90a97c9d057249796
Luke Robinson <asdluke1234@gmail.com>
2022-07-25 16:55:49 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const routes = require('./routes')
+const db = require('./db')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+
+app.use('/', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
