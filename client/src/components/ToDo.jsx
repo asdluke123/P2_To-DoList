47f1fd33aa87ca0799d539cc55297efdd43be9cd
Luke Robinson <asdluke1234@gmail.com>
2022-07-25 19:27:24 +0200

    Create ToDo components

--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -1,1 +1,9 @@
-const ToDo = ({todo,updateComplete,update})
+const ToDo = ({todo,updateComplete,updateFunction,removeFunction,updateFavorite}) => {
+    <div className="todo">
+        <input type={radio} onClick = {() => updateComplete(todo._id)}></input>
+        <p>{todo.toDo}</p>
+        <button onClick={() => updateFunction(todo._id)}>Update</button>
+        <button onClick={() => removeFunction(todo._id)}>Update</button>
+        <input type={radio} onClick = {() => updateFavorite(todo._id)}></input>
+    </div>
+}
