0c7ba1065b3ce523e3568f419fe56abe8e8de4c9
Luke Robinson <asdluke1234@gmail.com>
2022-07-25 16:51:38 +0200

    Create post routes

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/folders/:id',controllers.getListbyFolderId)
 router.get('/list/:id',controllers.getTodoByListId)
 router.get('/todo/:search',controllers.getTodoBySearch)
 
+router.post('/folders',controllers.createFolder)
+router.post('/folders/:id',controllers.createList)
+router.post('/list/:id',controllers.createToDo)
 
 
 
